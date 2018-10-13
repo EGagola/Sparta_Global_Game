96f4f08da6c4c17546c7626f4fcf596381befd26
Elliot Gagola <egagola@spartaglobal.com>
2018-10-13 19:49:29 +0100

    Added property tiles

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-  
+start();
 })
+var diceNum = ;
+
+function diceRoll() {
+  diceNum = Math.ceil((Math.random()*6);
+  document.getElementById('rollNum').innerHTML = diceNum;
+  console.log(diceNum);
+}
+diceRoll();
+
+var boardPositions = [];
+var redPiece, bluePiece;
+
+// function gamePiece(width, height, color, x, y) {
+//   this.width = width;
+//   this.height = height;
+//   this.color = color;
+//   this.x = x;
+//   this.y = y;
+// }
+// function start(){
+//   redPiece = new gamePiece(20,20,"red",10,10);
+//   bluePiece = new gamePiece(20,20,"blue",40,10);
+// }
