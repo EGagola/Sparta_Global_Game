4c79342134244a052e11c696a9f4b85489987a7d
Elliot Gagola <egagola@spartaglobal.com>
2018-10-14 01:00:57 +0100

    Added dice roll function and player turn UI, working on function to make elements based on dice value and set array of ID's for each board position

--- js/index.js
+++ js/index.js
@@ -1,16 +1,48 @@
+var diceNum;
+var turnCount = 0;
 document.addEventListener('DOMContentLoaded', function() {
-start();
-})
-var diceNum = ;
-
-function diceRoll() {
-  diceNum = Math.ceil((Math.random()*6);
+var start = document.getElementById('startButton');
+start.addEventListener("click",function() {
+  diceNum = Math.ceil(Math.random()*6);
   document.getElementById('rollNum').innerHTML = diceNum;
   console.log(diceNum);
-}
-diceRoll();
+  turnCount += 1;
+  if (turnCount % 2 ==0) {
+    console.log("Player 1 turn");
+    document.querySelector(".player_1_col").style.backgroundColor = "lightblue";
+    document.querySelector(".player_2_col").style.backgroundColor = "white";
+  } else if (turnCount % 2 ==1) {
+    console.log("Player 2 turn");
+    document.querySelector(".player_1_col").style.backgroundColor = "white";
+    document.querySelector(".player_2_col").style.backgroundColor = "lightblue";
+  }else {
+    console.log("Argghhhhh");
+  }
+  console.log(turnCount);
+  switch (diceNum) {
+    case (diceNum == 1):
+        console.log("move to old kent road");
+      break;
+    case (diceNum == 2):
+        console.log("move to whitechapel_road");
+      break;
+    case (diceNum == 3):
+        console.log("move to amgel");
+      break;
+    default:
+    console.log("Don't move");
+
+
+  }
+})
+})
+
 
 var boardPositions = [];
+boardPositions[0] = document.getElementsByClassName('old_kent_road');
+boardPositions[1] = document.getElementsByClassName('whitechapel_road');
+boardPositions[2] = document.getElementsByClassName('angel_islington');
+
 var redPiece, bluePiece;
 
 // function gamePiece(width, height, color, x, y) {
